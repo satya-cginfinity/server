0b03ff179cbd056fdd8275d041679838a9341448
satya-carextech <satyajeet.gupta@carextech.com>
2024-05-15 13:43:45 +0530

    added API to get user details

--- home.js
+++ home.js
@@ -1,4 +1,5 @@
 //#region Libraries
+require('dotenv').config();
 var express = require('express');
 var router = express.Router();
 var util = require('./common');
@@ -14,6 +15,18 @@ router.get('/homePageMessage', (req, res) => {
     var token = req.header('access-token');
     var user = util.getuserdetails(token); 
     res.json({ message: "Welcome "+ user.name +". Your email id is " + user.email + "." });
+});
+
+router.get('/userdetails', (req, res) => {
+    var token = req.header('access-token');
+    var user = util.getuserdetails(token); 
+    res.json
+        ({
+            name: user.name,
+            firstName: user.given_name,
+            email: user.email,
+            message: process.env.AUTHENTICATED
+        });
 });
 
 router.post('/api/stuff', (req, res, next) => {
